fa5bcecb3744f122c1de67b00154b6770655a0b0
David Da Silva <contact@daviddasilva.net>
2013-12-11 14:22:31 +0100

    2 types de comptes (+ compte clients)
    1000 clients avec leur compte courant. Need LazyLoading!

---
+++ GestionnaireDeComptesBanquaires-ejb/src/java/entities/Client.java
@@ -0,0 +1,113 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author shepard
+ */
+@Entity
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String nom;
+    private String password;
+    
+    @OneToMany(cascade = CascadeType.PERSIST)
+    private List<CompteBancaire> comptes;
+    
+
+    public Client() {}
+    
+    public Client(String nom, String password){
+        this.nom = nom;
+        this.setPassword(password);
+        this.comptes = new ArrayList();
+    }
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        //TODO : convertir en SHA1 ou MD5
+        this.password = password;
+    }
+
+    public List<CompteBancaire> getComptes() {
+        return comptes;
+    }
+
+    public void setComptes(List<CompteBancaire> comptes) {
+        this.comptes = comptes;
+    }
+
+    public void addCompte(CompteBancaire c){
+        this.comptes.add(c);
+    }
+    
+    public void removeCompte(CompteBancaire c){
+        this.comptes.remove(c);
+    }
+    
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Client)) {
+            return false;
+        }
+        Client other = (Client) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Client[ id=" + id + " ]";
+    }
+    
+}
